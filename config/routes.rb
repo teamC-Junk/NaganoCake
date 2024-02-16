b3b8f05464643d30eca8fc3aeb1abca5b3f79f2a
am121-dwc <aoimorisugi121@gmail.com>
2024-02-16 08:54:43 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,31 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
+  namespace :public do
+    root to: "homes#top"
+    get "about"=>"homes#about"
+    resources :items, only: [:index, :show]
+    get '/customers/my_page' => 'customers#show'
+    get '/customers/information/edit' => 'customers#edit'
+    patch '/customers/information' => 'customers#update'
+    get '/customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :orders, only: [:new, :create, :index, :show]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/complete' => 'orders#complete'
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy], path: '/addresses'
+  end
+  namespace :admin do
+    resources :order_details, only: [:update]
+    resources :orders, only: [:show, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    root to: 'homes#top'
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
