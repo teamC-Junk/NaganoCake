753416e8eb74a4156f9ffb861ca806a4375e3a91
am121-dwc <aoimorisugi121@gmail.com>
2024-02-16 04:48:13 +0000

    create view

---
+++ db/migrate/20240216042839_create_customers.rb
@@ -0,0 +1,18 @@
+class CreateCustomers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :customers do |t|
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :email
+      t.string :encrypted_password
+      t.string :post_code
+      t.string :address
+      t.string :phone_number
+      t.boolean :is_active
+
+      t.timestamps
+    end
+  end
+end
