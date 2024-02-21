80976bcdf064cf1a248b8f4e5989f832d2e1b69d
MasatoKato <kanperi808@gmail.com>
2024-02-21 10:34:42 +0000

    【Add] emailとpassword修正

--- db/migrate/20240216042839_create_customers.rb
+++ db/migrate/20240216042839_create_customers.rb
@@ -5,12 +5,18 @@ class CreateCustomers < ActiveRecord::Migration[6.1]
       t.string :first_name
       t.string :last_name_kana
       t.string :first_name_kana
-      # t.string :email
-      # t.string :encrypted_password
       t.string :post_code
       t.string :address
       t.string :phone_number
       t.boolean :is_active, default: true
+      
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.index ["email"], name: "index_customers_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -78,8 +78,15 @@ ActiveRecord::Schema.define(version: 2024_02_16_043535) do
     t.string "address"
     t.string "phone_number"
     t.boolean "is_active", default: true
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
