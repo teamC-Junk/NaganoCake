470db30c6ab64ac4211529a660a40d67947928a0
MasatoKato <kanperi808@gmail.com>
2024-02-21 16:15:23 +0000

    【Add] 会員詳細画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,10 @@
 class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.page(params[:page]).per(10)
-    
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
