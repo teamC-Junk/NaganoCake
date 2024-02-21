ac70bf1daf0bd9540dbaebeb01eb32a380126850
am121-dwc <aoimorisugi121@gmail.com>
2024-02-21 07:58:13 +0000

    unfinished

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,8 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
+    # @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price}
   end
 
   def update
@@ -12,5 +15,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+  end
+  private
+  def cart_items_params
+    params.require(:cart_items).permit(:amount)
   end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,9 @@
 class CartItem < ApplicationRecord
   belongs_to :customer  # 会員との関連付け (1対多)
   belongs_to :item      # 商品との関連付け (1対多)
+
+  validates :item_id, :amount, presence: true
+ def sum_of_price
+   item.price * amount
+ end
 end
