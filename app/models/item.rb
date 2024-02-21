df7ce3f017ca3a5d3a49cd6e0a116b96c27dfca4
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-02-21 10:09:33 +0000

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,13 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy # 注文商品との関連付け (1対多)
   has_one_attached :image
   validates :name, :introduction, :price, :image, presence: true
+  
+  def get_image(width, height) 
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end 
+      image.variant(resize_to_limit: [width,height]).processed
+  end 
 
 end
