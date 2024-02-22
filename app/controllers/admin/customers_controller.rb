24735e04ffade67cbf10c3462f120be6cdf73c6a
MasatoKato <kanperi808@gmail.com>
2024-02-22 04:33:26 +0000

    【Add] 会員編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,8 +8,24 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    
+    if @customer.update(customer_params)
+      flash[:notice] = "更新が完了しました。"
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
   end
+  
+  private
+  # 編集データのストロングパラメータ
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email, :is_active)
+  end
+  
 end
