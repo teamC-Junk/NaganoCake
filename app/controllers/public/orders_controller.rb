5f48f205ce2284ef88cd02c1e4b3e4d5a2c9eab8
MasatoKato <kanperi808@gmail.com>
2024-02-23 05:29:01 +0000

    【Add] 注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,48 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+    @customer_info = current_customer
   end
 
   def confirm
+    @order = Order.new(order_params)
+    # new 画面から渡ってきたデータ
+    @customer_info = current_customer
+  
+    if params[:order][:address_number] == "self_address"
+      # view で定義している address_number が"self_address"
+      @order.name = current_customer.last_name + current_customer.first_name
+      @order.address = current_customer.address
+      @order.post_code = current_customer.post_code
+    elsif params[:order][:address_number] == "registered_address"
+      # view で定義している address_number が"registered_address"
+      address = Address.find_by(id: params[:order][:address_id])
+      # view で定義している address_number が"registered_address"
+      if address.present?
+        @order.name = address.name
+        @order.address = address.address
+      else
+        flash.now[:error] = "選択された登録済み住所は存在しません"
+        render :new
+      end
+    elsif params[:order][:address_number] == "new_address"
+      # view で定義している address_number が"new_address"
+      address_new = current_customer.addresses.new(address_params)
+      if address_new.save
+        
+      else
+        render :new
+        # ここに渡ってくるデータはユーザーで新規追加してもらうので、入力不足の場合は new に戻します
+      end
+    else
+      render :new
+      # バグで上記以外の条件が発生した場合の処理
+    end
+
+    @cart_items = current_customer.cart_items.all # カートアイテムの情報をユーザーに確認してもらうために使用します
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_price }
+    # 合計金額を出す処理です sum_price はモデルで定義したメソッドです
+
   end
 
   def complete
@@ -15,5 +55,15 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+  
+  
+  private
+  def order_params
+    params.require(:order).permit(:name, :address, :total_payment)
+  end
+
+  def address_params
+    params.require(:order).permit(:name, :address, :post_code)
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,8 @@ class Address < ApplicationRecord
   validates :post_code, presence: true
   validates :address, presence: true
   validates :name, presence: true
+  
+  def address_display
+    '〒' + post_code + ' ' + address + ' ' + name
+  end
 end
