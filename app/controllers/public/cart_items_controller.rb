2d048ff3a109ae4b76318cb2a477498a060e3924
am121-dwc <aoimorisugi121@gmail.com>
2024-02-23 07:00:16 +0000

    save

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,7 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.all
-    # @cart_items = current_customer.cart_items.all
+    @cart_items = current_customer.cart_items.all
     @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price}
   end
 
@@ -11,22 +10,37 @@ class Public::CartItemsController < ApplicationController
     redirect_to request.referer
 
   end
-
-  def destroy
-    @cart_item.destroy
-    redirect_to request.referer, notice: "カート内商品の一つを削除しました。"
-  end
-
   def destroy_all
     CartItem.destroy_all
     redirect_to request.referer
   end
 
-  def create
+
+
+  def destroy
+    @cart_item = CartItem.find(params[:item_id])
+    @cart_item.destroy
+    redirect_to request.referer, notice: "カート内商品の一つを削除しました。"
   end
-  
+
+
+  def create
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.customer_id = current_customer.id
+    cart_item.item_id = cart_item_params[:item_id]
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.update(amount: cart_item.amount)
+      redirect_to cart_items_path
+    else
+      cart_item.save
+      redirect_to cart_items_path
+    end
+  end
+
   private
-  def cart_items_params
-    params.require(:cart_items).permit(:amount, :item_id)
+  def cart_item_params
+    params.require(:cart_item).permit(:amount, :item_id, :customer_id, :image)
   end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,18 @@ class CartItem < ApplicationRecord
   belongs_to :item      # 商品との関連付け (1対多)
 
   validates :item_id, :amount, presence: true
- def sum_of_price
-   item.price * amount
- end
+
+  has_one_attached :image
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [100,100]).processed
+  end
+
+  def sum_of_price
+    item.price * amount
+  end
 end
