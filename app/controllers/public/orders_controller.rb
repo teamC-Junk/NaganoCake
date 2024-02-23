95ba4c19b5b29a77e834cfe3b5e937e97014ece0
MasatoKato <kanperi808@gmail.com>
2024-02-23 15:25:51 +0000

    【Add] 注文情報確認

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,43 +6,37 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    # new 画面から渡ってきたデータ
     @customer_info = current_customer
+    
+    @order.shipping_fee = 800   # 送料
+    @total_price = 0            # 商品合計計算用
+    @consumption_tax = 1.1      # 消費税
   
-    if params[:order][:address_number] == "self_address"
-      # view で定義している address_number が"self_address"
+    if params[:order][:select_address] == "self_address"
       @order.name = current_customer.last_name + current_customer.first_name
       @order.address = current_customer.address
       @order.post_code = current_customer.post_code
-    elsif params[:order][:address_number] == "registered_address"
-      # view で定義している address_number が"registered_address"
+    elsif params[:order][:select_address] == "registered_address"
       address = Address.find_by(id: params[:order][:address_id])
-      # view で定義している address_number が"registered_address"
       if address.present?
         @order.name = address.name
         @order.address = address.address
+        @order.post_code = address.post_code
       else
         flash.now[:error] = "選択された登録済み住所は存在しません"
         render :new
       end
-    elsif params[:order][:address_number] == "new_address"
-      # view で定義している address_number が"new_address"
+    elsif params[:order][:select_address] == "new_address"
       address_new = current_customer.addresses.new(address_params)
       if address_new.save
-        
       else
-        render :new
-        # ここに渡ってくるデータはユーザーで新規追加してもらうので、入力不足の場合は new に戻します
+        render :new # 入力不足の場合の処理
       end
     else
-      render :new
-      # バグで上記以外の条件が発生した場合の処理
+      render :new # バグで上記以外の条件が発生した場合の処理
     end
-
-    @cart_items = current_customer.cart_items.all # カートアイテムの情報をユーザーに確認してもらうために使用します
-    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_price }
-    # 合計金額を出す処理です sum_price はモデルで定義したメソッドです
-
+    
+    @cart_items = current_customer.cart_items.all
   end
 
   def complete
@@ -57,10 +51,9 @@ class Public::OrdersController < ApplicationController
   def show
   end
   
-  
   private
   def order_params
-    params.require(:order).permit(:name, :address, :total_payment)
+    params.require(:order).permit(:name, :address, :post_code, :payment_method)
   end
 
   def address_params
